dba21b9848a62c53d563f92fdeef098c4fdc3d55
Seonwoo Choi <seonwoo31@dgu.ac.kr>
2025-09-23 02:30:54 +0900

    ♻️ (#316) fix: 아이디 기반 리팩토링 완료

--- src/config/mapConfigs.js
+++ src/config/mapConfigs.js
@@ -9,26 +9,25 @@ import DetailMapLaw from "../assets/images/icons/detailmap-icons/DetailMapLaw.pn
 
 // 건물별 상세 지도 + 날짜별 버튼 좌표
 export const mapConfigs = {
-만해광장: {
-  img: DetailMapManhae,
-  schedules: {
-   
-    "2024-09-25:day": [
-      { label: "갤럭시아머니트리", x: 40, y: 50 },
-      { label: "스타벅스 RTD", x: 80, y: 50 },
-    ],
-        "2024-09-25:night": [
-      { label: "FC 엘레펜테", x: 50, y: 50 },
-      { label: "끼리끼리", x: 70, y: 70 },
-    ],
-    "2024-09-26:day": [
-      { label: "이공헤르츠", x: 35, y: 40 },
-      { label: "레드불", x: 33, y: 60 },
-    ],
+  11: { // 만해광장
+    img: DetailMapManhae,
+    schedules: {
+      "2024-09-25:day": [
+        { label: "갤럭시아머니트리", x: 40, y: 50 },
+        { label: "스타벅스 RTD", x: 80, y: 50 },
+      ],
+      "2024-09-25:night": [
+        { label: "FC 엘레펜테", x: 50, y: 50 },
+        { label: "끼리끼리", x: 70, y: 70 },
+      ],
+      "2024-09-26:day": [
+        { label: "이공헤르츠", x: 35, y: 40 },
+        { label: "레드불", x: 33, y: 60 },
+      ],
+    },
   },
-},
 
-  사회과학관: {
+  8: { // 사회과학관
     img: DetailMapSocSci,
     schedules: {
       "2024-09-24:night": [
@@ -36,19 +35,18 @@ export const mapConfigs = {
         { label: "광고홍보학과", x: 60, y: 50 },
         { label: "행정학과", x: 85, y: 50 },
       ],
-       "2024-09-25:night": [
+      "2024-09-25:night": [
         { label: "행정학과", x: 70, y: 30 },
         { label: "프론티어", x: 60, y: 50 },
         { label: "정치외교학과", x: 85, y: 50 },
       ],
-       "2024-09-26:night": [
+      "2024-09-26:night": [
         { label: "북한학과", x: 70, y: 30 },
-     
       ],
     },
   },
 
-  팔정도: {
+  7: { // 팔정도
     img: DetailMapPaljeongdo,
     schedules: {
       "2024-09-24:day": [
@@ -58,35 +56,33 @@ export const mapConfigs = {
         { label: "사다리 극락정토", x: 85, y: 47 },
         { label: "축기단 굿즈부스", x: 85, y: 60 },
       ],
-            "2024-09-24:night": [
+      "2024-09-24:night": [
         { label: "에코코", x: 34, y: 25 },
-  
       ],
-        "2024-09-25:day": [
+      "2024-09-25:day": [
         { label: "그래픽커뮤니케이션 사이언스", x: 34, y: 25 },
         { label: "키운셀러", x: 75, y: 25 },
         { label: "동국이네 슈퍼마켓 자연순환보증금관리센터 x 코카콜라", x: 20, y: 55 },
         { label: "디그램", x: 50, y: 60 },
         { label: "축기단 굿즈부스", x: 85, y: 55 },
       ],
-        "2024-09-25:night": [
+      "2024-09-25:night": [
         { label: "에코코", x: 34, y: 25 },
       ],
-              "2024-09-26:day": [
+      "2024-09-26:day": [
         { label: "그래픽커뮤니케이션 사이언스", x: 34, y: 25 },
         { label: "키운셀러", x: 75, y: 25 },
         { label: "108리더스 상록수커피클럽", x: 20, y: 60 },
         { label: "사다리 극락정토", x: 85, y: 47 },
         { label: "축기단 굿즈부스", x: 85, y: 60 },
       ],
-        "2024-09-26:night": [
+      "2024-09-26:night": [
         { label: "에코코", x: 34, y: 25 },
-
       ],
     },
   },
 
-  명진관: {
+  5: { // 명진관
     img: DetailMapMyungjin,
     schedules: {
       "2024-09-24:night": [
@@ -94,17 +90,17 @@ export const mapConfigs = {
         { label: "국어국문문예창작학부", x: 55, y: 60 },
         { label: "철학과", x: 80, y: 60 },
       ],
-        "2024-09-25:night": [
+      "2024-09-25:night": [
         { label: "참사랑 봉사단", x: 55, y: 60 },
         { label: "푸름누리", x: 80, y: 60 },
       ],
-         "2024-09-26:night": [
+      "2024-09-26:night": [
         { label: "FC 엘레펜테", x: 55, y: 60 },
       ],
     },
   },
 
-  다향관: {
+  3: { // 다향관
     img: DetailMapDahyang,
     schedules: {
       "2024-09-24:day": [
@@ -112,17 +108,20 @@ export const mapConfigs = {
       ],
       "2024-09-24:night": [
         { label: "사범대학", x: 50, y: 75 },
-      ],"2024-09-25:day": [
+      ],
+      "2024-09-25:day": [
         { label: "플리마켓", x: 50, y: 75 },
-      ],"2024-09-26:day": [
+      ],
+      "2024-09-26:day": [
         { label: "플리마켓", x: 50, y: 75 },
-      ],"2024-09-26:night": [
+      ],
+      "2024-09-26:night": [
         { label: "체육교육과", x: 50, y: 75 },
       ],
     },
   },
 
-  원흥관: {
+  12: { // 원흥관
     img: DetailMapWonheung,
     schedules: {
       "2024-09-24:night": [
@@ -131,23 +130,23 @@ export const mapConfigs = {
     },
   },
 
-  혜화관: {
+  17: { // 혜화관
     img: DetailMapHyehwa,
     schedules: {
-         "2024-09-24:night": [
+      "2024-09-24:night": [
         { label: "디프", x: 50, y: 50 },
       ],
       "2024-09-25:night": [
         { label: "디프", x: 50, y: 50 },
         { label: "경영학과", x: 70, y: 50 },
       ],
-          "2024-09-26:night": [
+      "2024-09-26:night": [
         { label: "디프", x: 50, y: 50 },
       ],
     },
   },
 
-  "만해/법학관": {
+  4: { // 만해/법학관
     img: DetailMapLaw,
     schedules: {
       "2024-09-24:day": [
@@ -158,24 +157,21 @@ export const mapConfigs = {
         { label: "테스트부스1", x: 50, y: 70 },
         { label: "야구부 프런트 다독다독", x: 50, y: 30 },
       ],
-         "2024-09-25:day": [
+      "2024-09-25:day": [
         { label: "더플레이", x: 50, y: 70 },
         { label: "야구부 프런트 다독다독", x: 50, y: 30 },
         { label: "인액터스", x: 50, y: 30 },
-
       ],
-         "2024-09-25:night": [
+      "2024-09-25:night": [
         { label: "불교 대학", x: 50, y: 70 },
         { label: "법과 대학", x: 50, y: 30 },
       ],
-              "2024-09-26:day": [
+      "2024-09-26:day": [
         { label: "메이투", x: 50, y: 70 },
- 
-
       ],
-         "2024-09-26:night": [
+      "2024-09-26:night": [
         { label: "첨단융합대학", x: 50, y: 70 },
       ],
     },
   },
-}; 
+};
