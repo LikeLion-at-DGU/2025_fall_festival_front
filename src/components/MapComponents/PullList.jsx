00919073cc8621550da77a46cb35dbd74d65204d
Seonwoo Choi <seonwoo31@dgu.ac.kr>
2025-09-24 08:44:27 +0900

    🐛 (#316) fix: 메타태그, 바텀시트 수정

--- src/components/MapComponents/PullList.jsx
+++ src/components/MapComponents/PullList.jsx
@@ -28,7 +28,7 @@ function PullList({
 
   // ✅ maxHeight를 상태로 관리
   const [maxHeight, setMaxHeight] = useState(
-    Math.min(412, window.innerHeight - 100 - 82)
+    Math.min(410, window.innerHeight - 100 - 82)
   );
 
   useEffect(() => {
